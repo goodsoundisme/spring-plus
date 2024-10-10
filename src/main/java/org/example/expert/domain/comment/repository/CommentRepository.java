624d77036a2b443de86f413455e1c12f6e2bba98
goodsoundisme <sol000619@naver.com>
2024-10-10 09:20:46 +0900

    [update] level 2-2 N+1 문제. fetch join 이용

--- src/main/java/org/example/expert/domain/comment/repository/CommentRepository.java
+++ src/main/java/org/example/expert/domain/comment/repository/CommentRepository.java
@@ -11,4 +11,10 @@ public interface CommentRepository extends JpaRepository<Comment, Long> {
 
     @Query("SELECT c FROM Comment c JOIN c.user WHERE c.todo.id = :todoId")
     List<Comment> findByTodoIdWithUser(@Param("todoId") Long todoId);
+
+    //comment를 가져올 때, user도 가져오게 하는
+    @Query("SELECT c FROM Comment c"+
+    "JOIN FETCH c.user"+ //user를 fetch join으로 즉시로딩시킴
+    "WHERE c.todo.id =:todoId")
+    List<Comment> findByTodoIdWithUser(@Param("todoId") long todoId);
 }
